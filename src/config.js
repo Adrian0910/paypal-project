9946092dc3e2328e1f23df6a3e812f2616a9100d
Adrian Ibarra <35971687+Adrian0910@users.noreply.github.com>
2024-11-14 22:47:18 -0600

    Cambios backend

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ export const PORT = 3000;
 export const CLIENT_PAYPAL = process.env.CLIENT_PAYPAL_ID;
 export const SECRET_PAYPAL = process.env.SECRET_PAYPAL_ID;
 export const PAYPAL_API = `https://api-m.sandbox.paypal.com`
-export const HOST = 'https://videojuegos-project-adrian.netlify.app';
+//export const HOST = 'https://videojuegos-project-adrian.netlify.app';
