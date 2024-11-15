a8cdfd4bc5b49ae6365ffc35243e4fd50282dd0c
Adrian0910 <adrianibarra60@aragon.unam.mx>
2024-11-15 12:04:08 -0600

    new change
    cange

--- src/config.js
+++ src/config.js
@@ -5,4 +5,5 @@ export const PORT = 3000;
 export const CLIENT_PAYPAL = process.env.CLIENT_PAYPAL_ID;
 export const SECRET_PAYPAL = process.env.SECRET_PAYPAL_ID;
 export const PAYPAL_API = `https://api-m.sandbox.paypal.com`
-export const HOST = 'http://localhost:3000';
+//export const HOST = 'http://localhost:3000';
+export const HOST = 'paypal-project-production.up.railway.app';
