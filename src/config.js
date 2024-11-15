957093cf60f6d3fded49cacfc0cf5ccdea9949f9
Adrian0910 <adrianibarra60@aragon.unam.mx>
2024-11-15 12:16:26 -0600

    c
    c

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const CLIENT_PAYPAL = process.env.CLIENT_PAYPAL_ID;
 export const SECRET_PAYPAL = process.env.SECRET_PAYPAL_ID;
 export const PAYPAL_API = `https://api-m.sandbox.paypal.com`
 //export const HOST = 'http://localhost:3000';
-export const HOST = 'paypal-project-production.up.railway.app/';
+export const HOST = 'paypal-project-production.up.railway.app.*';
